df0574b74dde94a461788408573bf13e947ba700
sangxxjin <sj_0368@naver.com>
2024-11-26 12:08:33 +0900

    blue 2

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -3,9 +3,9 @@ package org.example;
 public class Calc {
 
     public static int run(String expr) {
-        if ("10 + 5".equals(expr)) {
-            return 15;
-        }
-        return 8;
+        String[] exprBits = expr.split(" \\+ ");
+        int num1 = Integer.parseInt(exprBits[0]);
+        int num2 = Integer.parseInt(exprBits[1]);
+        return num1 + num2;
     }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -11,7 +11,7 @@ public class CalcTest {
     @Test
     @DisplayName("3 + 5")
     public void t1() {
-        int rs = Calc.run("3+5");
+        int rs = Calc.run("3 + 5");
         assertThat(rs).isEqualTo(8);
     }
     @Test
