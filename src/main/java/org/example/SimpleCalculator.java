1214114b70cb518b35d9ecb7ae1575cd737e082a
sangxxjin <sj_0368@naver.com>
2024-11-26 11:17:28 +0900

    feat: multiply

--- src/main/java/org/example/SimpleCalculator.java
+++ src/main/java/org/example/SimpleCalculator.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class SimpleCalculator {
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
     public int plus(int a, int b) {
         return a + b;
     }
