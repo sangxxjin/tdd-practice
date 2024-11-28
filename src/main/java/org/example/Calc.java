4c1859393fec76c9de83d9ceed08ce065a71c518
sangxxjin <sj_0368@naver.com>
2024-11-28 14:30:30 +0900

    reset

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,16 +1,6 @@
 package org.example;
 
+
 public class Calc {
 
-    public static int run(String expr) {
-        return typeCalc(expr);
-    }
-
-    private static int typeCalc(String expr) {
-        if (expr.contains(" + ")) {
-            String[] split = expr.split(" \\+ ");
-            return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
-        }
-        return 0;
-    }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,31 +1,5 @@
 package org.example;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 public class CalcTest {
-    // 최종 목표 Calc.run("((3 + 5) * 5 + -10) * 10 / 5");
-
-    @Test
-    @DisplayName("3 + 5")
-    public void t1() {
-        int rs = Calc.run("3 + 5");
-        assertThat(rs).isEqualTo(8);
-    }
-    @Test
-    @DisplayName("10 + 5")
-    public void t2() {
-        int rs = Calc.run("10 + 5");
-        assertThat(rs).isEqualTo(15);
-    }
-
-    @Test
-    @DisplayName("(3 + 5) * 5")
-    public void t3() {
-        int rs = Calc.run("(3 + 5) * 5");
-        assertThat(rs).isEqualTo(40);
-    }
 
 }
