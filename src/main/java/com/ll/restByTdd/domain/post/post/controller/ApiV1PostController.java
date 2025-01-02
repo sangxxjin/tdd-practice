36734e23f69974597c80444200e17d7d01b79fe5
sangxxjin <sj_0368@naver.com>
2025-01-02 14:19:01 +0900

    tg : POST /api/v1/posts

--- src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
@@ -1,21 +1,52 @@
 package com.ll.restByTdd.domain.post.post.controller;
 
+import com.ll.restByTdd.domain.member.member.entity.Member;
 import com.ll.restByTdd.domain.post.post.dto.PostDto;
+import com.ll.restByTdd.domain.post.post.entity.Post;
 import com.ll.restByTdd.domain.post.post.service.PostService;
+import com.ll.restByTdd.global.rq.Rq;
+import com.ll.restByTdd.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
+    private final Rq rq;
 
     @GetMapping("/{id}")
-    public PostDto item(@PathVariable Long id) {
+    public PostDto item(@PathVariable long id) {
         return new PostDto(postService.findById(id).get());
+    }
+
+
+    record PostWriteReqBody(
+        @NotBlank
+        @Length(min = 2)
+        String title,
+        @NotBlank
+        @Length(min = 2)
+        String content
+    ) {
+    }
+
+    @PostMapping
+    public RsData<PostDto> write(
+        @RequestBody @Valid PostWriteReqBody reqBody
+    ) {
+        Member actor = rq.checkAuthentication();
+
+        Post post = postService.write(actor, reqBody.title, reqBody.content);
+
+        return new RsData<>(
+            "201-1",
+            "%d번 글이 작성되었습니다.".formatted(post.getId()),
+            new PostDto(post)
+        );
     }
 }
