89452df0ae583543f4afd9c2e25f5a54343d8265
sangxxjin <sj_0368@naver.com>
2025-01-02 12:51:52 +0900

    tr, tg : /api/v1/posts/1

---
+++ src/main/java/com/ll/restByTdd/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,21 @@
+package com.ll.restByTdd.domain.post.post.controller;
+
+import com.ll.restByTdd.domain.post.post.dto.PostDto;
+import com.ll.restByTdd.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class ApiV1PostController {
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    public PostDto item(@PathVariable Long id) {
+        return new PostDto(postService.findById(id).get());
+    }
+}
