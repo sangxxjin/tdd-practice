763fce52740ee4ca08f594e77273e6a4287bb129
sangxxjin <sj_0368@naver.com>
2025-01-03 16:11:25 +0900

    t, g, PUT /api/v1/posts/1/comments/1

--- src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/ll/restByTdd/domain/post/comment/controller/ApiV1PostCommentController.java
@@ -8,10 +8,15 @@ import com.ll.restByTdd.domain.post.post.service.PostService;
 import com.ll.restByTdd.global.exceptions.ServiceException;
 import com.ll.restByTdd.global.rq.Rq;
 import com.ll.restByTdd.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -54,6 +59,34 @@ public class ApiV1PostCommentController {
         return new RsData<>(
             "200-1",
             "%d번 댓글이 삭제되었습니다.".formatted(id)
+        );
+    }
+
+    record PostCommentModifyReqBody(
+        @NotBlank
+        @Length(min = 2, max = 100)
+        String content
+    ) {
+    }
+    @PutMapping("/{id}")
+    public RsData<PostCommentDto> modify(
+        @PathVariable long postId,
+        @PathVariable long id,
+        @RequestBody @Valid PostCommentModifyReqBody reqBody
+    ) {
+        Member actor = rq.checkAuthentication();
+        Post post = postService.findById(postId).orElseThrow(
+            () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
+        );
+        PostComment postComment = post.getCommentById(id).orElseThrow(
+            () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
+        );
+        postComment.checkActorCanModify(actor);
+        postComment.modify(reqBody.content);
+        return new RsData<>(
+            "200-1",
+            "%d번 댓글이 수정되었습니다.".formatted(id),
+            new PostCommentDto(postComment)
         );
     }
 }
