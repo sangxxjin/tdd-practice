973ce5dd4d539f8eb0f8662bc383e7e82a21191c
sangxxjin <sj_0368@naver.com>
2025-01-03 09:57:20 +0900

    t, r/g : DELETE /api/v1/posts/1, with no permission

--- src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/restByTdd/com/ll/restByTdd/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -364,5 +364,23 @@ public class ApiV1PostControllerTest {
             .andExpect(jsonPath("$.msg").value("apiKey를 입력해주세요."));
     }
 
+    @Test
+    @DisplayName("글 삭제, with no permission")
+    void t13() throws Exception {
+        Member actor = memberService.findByUsername("user2").get();
+        ResultActions resultActions = mvc
+            .perform(
+                delete("/api/v1/posts/1")
+                    .header("Authorization", "Bearer " + actor.getApiKey())
+            )
+            .andDo(print());
+        resultActions
+            .andExpect(handler().handlerType(ApiV1PostController.class))
+            .andExpect(handler().methodName("delete"))
+            .andExpect(status().isForbidden())
+            .andExpect(jsonPath("$.resultCode").value("403-2"))
+            .andExpect(jsonPath("$.msg").value("작성자만 글을 삭제할 수 있습니다."));
+    }
+
 
 }
